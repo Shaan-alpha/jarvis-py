6270deea1c622656120267ea0f8ba12083f2236f
Shaan Satsangi <shaansatsangi@gmail.com>
2026-05-13 08:51:37 +0530

    feat: add media control intent handler and update conversational memory for volume adjustments

--- core/intents/media_control.py
+++ core/intents/media_control.py
@@ -3,14 +3,38 @@ import pyautogui
 
 def handle_media_control(query, speak):
 
-    if "volume up" in query:
+    # Volume Up
+    if any(word in query for word in [
+        "volume up",
+        "increase volume",
+        "increase the volume",
+        "raise volume",
+        "raise the volume",
+    ]):
+
         pyautogui.press("volumeup")
+
         speak("Volume increased")
 
-    elif "volume down" in query:
+    # Volume Down
+    elif any(word in query for word in [
+        "volume down",
+        "decrease volume",
+        "decrease the volume",
+        "lower volume",
+    ]):
+
         pyautogui.press("volumedown")
+
         speak("Volume decreased")
 
-    elif "mute" in query:
+    # Mute
+    elif any(word in query for word in [
+        "mute",
+        "mute volume",
+        "volume mute",
+    ]):
+
         pyautogui.press("volumemute")
+
         speak("Volume muted")
